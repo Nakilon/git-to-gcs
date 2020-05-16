f30ab2f3024d4fadee59b15e7b30ee180534989d
Victor Maslov <nakilon@gmail.com>
2020-05-16 21:13:50 +0300

    git does not seem to store mtime -- the -c flag should make gsutil use md5 instead of mtime

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,3 @@
 FROM google/cloud-sdk:alpine
 COPY main.rb /main.rb
-CMD sh -c "cat /github/workflow/event.json && ls -la && apk add ruby && ruby /main.rb"
+CMD sh -c "apk add ruby && ruby /main.rb"
--- main.rb
+++ main.rb
@@ -5,5 +5,5 @@ Dir[?*].each do |bucket|
   File.write "/temp.json", Base64.decode64(Hash[*ENV["INPUT_SECRETS"].split].fetch bucket)
   `gcloud auth activate-service-account --key-file=/temp.json`
   `rm /temp.json`
-  `gsutil -m rsync -r -d #{bucket} gs://#{bucket}`
+  `gsutil -m rsync -r -c -d #{bucket} gs://#{bucket}`
 end
