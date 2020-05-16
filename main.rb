e7e59a47ba1f6edad97634dea85754957cdf5245
Victor Maslov <nakilon@gmail.com>
2020-05-16 20:47:09 +0300

    require 'base64'

--- main.rb
+++ main.rb
@@ -1,13 +1,9 @@
-require "pp"
-
-pp ENV["INPUT_SECRETS"][1,20]
+require "base64"
 
 Dir[?*].each do |bucket|
   next unless File.directory? bucket
-  puts "BUCKET '#{bucket}'"
   File.write "/temp.json", Base64.decode64(Hash[*ENV["INPUT_SECRETS"].split].fetch bucket)
   `gcloud auth activate-service-account --key-file=/temp.json`
   `rm /temp.json`
   `gsutil -m rsync -r -d #{bucket} gs://#{bucket}`
 end
-
