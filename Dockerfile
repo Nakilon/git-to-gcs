316e59d68372518047b683884dab84d82e788062
Victor Maslov <nakilon@gmail.com>
2020-05-16 20:38:23 +0300

    made the script

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,3 @@
 FROM google/cloud-sdk:alpine
 COPY main.rb /main.rb
-CMD sh -c "env && pwd && ls -l /github/home && ls -l /github/workflow && ls -l /github/workspace && apk add ruby && ruby main.rb"
+CMD sh -c "cat /github/workflow/event.json && ls -la && apk add ruby && ruby /main.rb"
--- main.rb
+++ main.rb
@@ -1,5 +1,13 @@
 require "pp"
 
-pp ENV["secrets"][1,20]
-abort
+pp ENV["INPUT_SECRETS"][1,20]
+
+Dir[?*].each do |bucket|
+  next unless File.directory? bucket
+  puts "BUCKET '#{bucket}'"
+  File.write "/temp.json", Base64.decode64(Hash[*ENV["INPUT_SECRETS"].split].fetch bucket)
+  `gcloud auth activate-service-account --key-file=/temp.json`
+  `rm /temp.json`
+  `gsutil -m rsync -r -d #{bucket} gs://#{bucket}`
+end
 
